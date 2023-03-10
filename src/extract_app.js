2be9842aa160a5f68c988e316d91bfc26bbc8f1b
Göran Sander <goran.sander@gmail.com>
2023-03-10 18:34:34 +0100

    feat: Create pre-built binaries for Windows, macOS and Linux Implements #69

--- src/extract_app.js
+++ src/extract_app.js
@@ -1,104 +1,107 @@
-var fs = require('fs-extra');
+const fs = require('fs-extra');
 const enigma = require('enigma.js');
 const WebSocket = require('ws');
-var Queue = require('better-queue');
-
+const config = require('config');
+const path = require('path');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 // Load our own code
-const logger = require('./logger.js');
-
-
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const path = require('path');
-var config = require('config');
+const logger = require('./logger');
 
 let processedApps = 0;
 
-
 // Helper function to read the contents of the certificate files:
 // const readCert = filename => fs.readFileSync(path.resolve(__dirname, certificatesPath, filename));
-const readCert = filename => fs.readFileSync(filename);
-
+const readCert = (filename) => fs.readFileSync(filename);
 
 //  Engine config
-var configEngine = {
+const configEngine = {
     engineVersion: config.get('ButlerSpyglass.configEngine.engineVersion'),
     host: config.get('ButlerSpyglass.configEngine.server'),
     port: config.get('ButlerSpyglass.configEngine.serverPort'),
-    isSecure: config.get('ButlerSpyglass.configEngine.isSecure'),
+    isSecure: config.get('ButlerSpyglass.configEngine.useSSL'),
     headers: config.get('ButlerSpyglass.configEngine.headers'),
-    ca: readCert(config.get('ButlerSpyglass.configEngine.ca')),
-    cert: readCert(config.get('ButlerSpyglass.configEngine.cert')),
-    key: readCert(config.get('ButlerSpyglass.configEngine.key')),
-    rejectUnauthorized: config.get('ButlerSpyglass.configEngine.rejectUnauthorized')
+    ca: readCert(config.get('ButlerSpyglass.cert.clientCertCA')),
+    cert: readCert(config.get('ButlerSpyglass.cert.clientCert')),
+    key: readCert(config.get('ButlerSpyglass.cert.clientCertKey')),
+    rejectUnauthorized: config.get('ButlerSpyglass.configEngine.rejectUnauthorized'),
 };
 
-
 // Set up enigma.js configuration
-const qixSchema = require('enigma.js/schemas/' + configEngine.engineVersion);
-
-
+// eslint-disable-next-line import/no-dynamic-require
+const qixSchema = require(`enigma.js/schemas/${configEngine.engineVersion}`);
 
 // -------------
 // Exports
 // -------------
-
 module.exports.resetExtractedAppCount = () => {
     processedApps = 0;
-}
+};
 
-module.exports.appExtractMetadata = async function (worker, queue, appToExtract, cb) {
-    
-    lineageFileWriter = createCsvWriter({
-        path: path.resolve(path.normalize(config.get('ButlerSpyglass.lineage.lineageFolder') + '/lineage.csv')),
-        header: [{
-                id: 'appId',
-                title: 'AppId'
-            },
-            {
-                id: 'discriminator',
-                title: 'Discriminator'
-            },
-            {
-                id: 'statement',
-                title: 'Statement'
-            }
-        ],
-        append: false
-    });
+module.exports.appExtractMetadata = async function appExtractMetadata(
+    worker,
+    queue,
+    appToExtract,
+    cb
+) {
+    // const lineageFileWriter = createCsvWriter({
+    //     path: path.resolve(
+    //         path.normalize(`${config.get('ButlerSpyglass.lineage.exportDir')}/lineage.csv`)
+    //     ),
+    //     header: [
+    //         {
+    //             id: 'appId',
+    //             title: 'AppId',
+    //         },
+    //         {
+    //             id: 'discriminator',
+    //             title: 'Discriminator',
+    //         },
+    //         {
+    //             id: 'statement',
+    //             title: 'Statement',
+    //         },
+    //     ],
+    //     append: false,
+    // });
 
     // Increase counter of # processed apps in this extraction run
-    processedApps++;
+    processedApps += 1;
 
     // Get queue stats
-    var queueStats = queue.getStats();
-    logger.logger.info(`Extracting metadata (#${processedApps}, overall success rate ${100*queueStats.successRate}%): ${appToExtract.qDocId} <<>> ${appToExtract.qTitle}`);
+    const queueStats = queue.getStats();
+    logger.logger.info(
+        `Extracting metadata (#${processedApps}, overall success rate ${
+            100 * queueStats.successRate
+        }%): ${appToExtract.qDocId} <<>> ${appToExtract.qTitle}`
+    );
 
     // Create a new session
-    let configEnigma = {
+    const configEnigma = {
         schema: qixSchema,
         url: `wss://${configEngine.host}:${configEngine.port}/app/${appToExtract.qDocId}`,
-        createSocket: url => new WebSocket(url, {
-            ca: [configEngine.ca],
-            key: configEngine.key,
-            cert: configEngine.cert,
-            headers: {
-                'X-Qlik-User': config.get('ButlerSpyglass.configEngine.headers.X-Qlik-User')
-            },
-            rejectUnauthorized: config.get('ButlerSpyglass.configEngine.rejectUnauthorized')
-        }),
+        createSocket: (url) =>
+            new WebSocket(url, {
+                ca: [configEngine.ca],
+                key: configEngine.key,
+                cert: configEngine.cert,
+                headers: configEngine.headers,
+                rejectUnauthorized: configEngine.rejectUnauthorized,
+            }),
         protocol: {
-            delta: false
-        }
+            delta: false,
+        },
     };
 
-    let session = enigma.create(configEnigma);
+    const session = enigma.create(configEnigma);
     let global;
 
     try {
         global = await session.open();
     } catch (err) {
-        logger.logger.error(`enigmaOpen error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`);
+        logger.logger.error(
+            `enigmaOpen error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`
+        );
 
         cb(err);
         return;
@@ -110,69 +113,125 @@ module.exports.appExtractMetadata = async function (worker, queue, appToExtract,
 
     try {
         app = await global.openDoc(appToExtract.qDocId, '', '', '', true);
-        logger.logger.debug('openDoc success for appId: ' + appToExtract.qDocId);
+        logger.logger.debug(`openDoc success for appId: ${appToExtract.qDocId}`);
     } catch (err) {
-        logger.logger.error(`openDoc error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`);
+        logger.logger.error(
+            `openDoc error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`
+        );
 
         session.close();
         cb(err);
         return;
     }
 
-
-    if (config.get('ButlerSpyglass.lineage.enableLineageExtract') == true) {
-
+    if (config.get('ButlerSpyglass.lineage.enableLineageExtract') === true) {
         try {
-            let lineageCurrentApp = [];
+            const lineageCurrentApp = [];
 
-            lineage = await app.getLineage();
-            logger.logger.debug('getLineage success for appId: ' + appToExtract.qDocId);
+            const lineage = await app.getLineage();
+            logger.logger.debug(`getLineage success for appId: ${appToExtract.qDocId}`);
             logger.logger.debug(JSON.stringify(lineage, null, 2));
 
             // Create CSV write for storing current app's lineage to disk
-            lineageCurrentAppWriter = createCsvWriter({
-                path: path.resolve(path.normalize(config.get('ButlerSpyglass.lineage.lineageFolder') + '/' + appToExtract.qDocId + '.csv')),
-                header: [{
+            const lineageCurrentAppWriter = createCsvWriter({
+                path: path.resolve(
+                    path.normalize(
+                        `${config.get('ButlerSpyglass.lineage.exportDir')}/${
+                            appToExtract.qDocId
+                        }.csv`
+                    )
+                ),
+                header: [
+                    {
                         id: 'appId',
-                        title: 'AppId'
+                        title: 'AppId',
+                    },
+                    {
+                        id: 'appName',
+                        title: 'AppName',
                     },
                     {
                         id: 'discriminator',
-                        title: 'Discriminator'
+                        title: 'Discriminator',
                     },
                     {
                         id: 'statement',
-                        title: 'Statement'
-                    }
+                        title: 'Statement',
+                    },
                 ],
-                append: false
+                append: false,
             });
 
-            lineage.forEach(element => {
-                if (element.qStatement == undefined) element.qStatement = '';
-
-                // Push lineage for current app into its own array, for immediate storage on disk
-                // Only extract first 1000 characters of the discriminator and statement data, respectively.
+            // If there is no lineage info for an app, we should still write appId and appName to the CSV/JSON file
+            if (lineage.length === 0) {
                 lineageCurrentApp.push({
                     appId: appToExtract.qDocId,
-                    discriminator: element.qDiscriminator.substring(0, config.get('ButlerSpyglass.lineage.maxLengthDiscriminator')),
-                    statement: element.qStatement.substring(0, config.get('ButlerSpyglass.lineage.maxLengthStatement'))
+                    appName: appToExtract.qDocName,
                 });
-            });
+            } else {
+                lineage.forEach((element) => {
+                    const el = element;
+                    if (el.qStatement === undefined) el.qStatement = '';
+                    if (el.qDiscriminator === undefined) el.qDiscriminator = '';
 
-            // Save lineage info to disk file
+                    // Push lineage for current app into its own array, for immediate storage on disk
+                    // Only extract first x characters of the discriminator and statement data, as specified in the config file.
+                    lineageCurrentApp.push({
+                        appId: appToExtract.qDocId,
+                        appName: appToExtract.qDocName,
+                        discriminator: el.qDiscriminator.substring(
+                            0,
+                            config.get('ButlerSpyglass.lineage.maxLengthDiscriminator')
+                        ),
+                        statement: el.qStatement.substring(
+                            0,
+                            config.get('ButlerSpyglass.lineage.maxLengthStatement')
+                        ),
+                    });
+                });
+            }
+
+            // Save lineage info to disk files
+            // First CSV
             lineageCurrentAppWriter
                 .writeRecords(lineageCurrentApp)
                 .then(() => {
-                    logger.logger.verbose(`Done writing ${lineageCurrentApp.length} lineage records for app ID ${appToExtract.qDocId} to disk file`);
+                    logger.logger.verbose(
+                        `Done writing ${lineageCurrentApp.length} lineage records for app ID ${appToExtract.qDocId} to CSV disk file`
+                    );
                 })
                 .catch((error) => {
-                    logger.logger.error(`Failed to write lineage info to disk for app ID ${appToExtract.qDocId} (make sure the output directory exists!): ${error}`);
+                    logger.logger.error(
+                        `Failed to write lineage info to CSV file on disk for app ID ${appToExtract.qDocId} (make sure the output directory exists!): ${error}`
+                    );
                     process.exit(1);
                 });
 
+            // Then as JSON
+            try {
+                fs.writeFileSync(
+                    path.resolve(
+                        path.normalize(
+                            `${config.get('ButlerSpyglass.lineage.exportDir')}/${
+                                appToExtract.qDocId
+                            }.json`
+                        )
+                    ),
+                    JSON.stringify(lineageCurrentApp, 0, 2)
+                );
+                logger.logger.verbose(
+                    `Done writing script for app ID ${appToExtract.qDocId} to disk`
+                );
+            } catch (err) {
+                logger.logger.error(
+                    `Failed to write lineage info to JSON file on disk for app ID ${appToExtract.qDocId} (make sure the output directory exists!): ${err}`
+                );
+                process.exit(2);
+            }
         } catch (err) {
-            logger.logger.error(`getLineage error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`);
+            logger.logger.error(
+                `getLineage error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`
+            );
 
             session.close();
             cb(err);
@@ -180,29 +239,40 @@ module.exports.appExtractMetadata = async function (worker, queue, appToExtract,
         }
     }
 
-    if (config.get('ButlerSpyglass.script.enableScriptExtract') == true) {
+    if (config.get('ButlerSpyglass.script.enableScriptExtract') === true) {
         try {
-            script = await app.getScript();
-            logger.logger.debug('getScript success for appId: ' + appToExtract.qDocId);
+            const script = await app.getScript();
+            logger.logger.debug(`getScript success for appId: ${appToExtract.qDocId}`);
             logger.logger.silly(script);
 
             // Save current app's script to disk file. Sync writing to keep things simple.
             try {
                 fs.writeFileSync(
-                    path.resolve(path.normalize(config.get('ButlerSpyglass.script.scriptFolder') + '/' + appToExtract.qDocId + '.qvs')),
+                    path.resolve(
+                        path.normalize(
+                            `${config.get('ButlerSpyglass.script.exportDir')}/${
+                                appToExtract.qDocId
+                            }.qvs`
+                        )
+                    ),
                     script
                 );
-                logger.logger.verbose(`Done writing script for app ID ${appToExtract.qDocId} to disk`);
+                logger.logger.verbose(
+                    `Done writing script for app ID ${appToExtract.qDocId} to disk`
+                );
             } catch (err) {
-                logger.logger.error(`Error when writing script for app ID ${appToExtract.qDocId} to disk: ${err}`);
+                logger.logger.error(
+                    `Error when writing script for app ID ${appToExtract.qDocId} to disk: ${err}`
+                );
                 session.close();
                 cb(err);
                 return;
             }
-
         } catch (err) {
             session.close();
-            logger.logger.error(`getScript error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`);
+            logger.logger.error(
+                `getScript error (app ID=${appToExtract.qDocId}): ${JSON.stringify(err)}`
+            );
             session.close();
 
             cb(err);
@@ -218,6 +288,5 @@ module.exports.appExtractMetadata = async function (worker, queue, appToExtract,
         logger.logger.error(`Error when closing session (app ID=${appToExtract.qDocId}): ${err}`);
 
         cb(err);
-        return;
     }
-}
+};
---
+++ src/get_dataconnection.js
@@ -0,0 +1,110 @@
+const fs = require('fs-extra');
+const config = require('config');
+const path = require('path');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const QrsInteract = require('qrs-interact');
+
+// Load our own code
+const { logger } = require('./logger');
+
+// -------------
+// Exports
+// -------------
+module.exports.getDataConnections = async function getDataConnections() {
+    try {
+        // Create CSV write for storing current app's lineage to disk
+        const dataconnectionsWriter = createCsvWriter({
+            path: path.resolve(
+                path.normalize(
+                    `${config.get('ButlerSpyglass.dataconnection.exportDir')}/dataconnections.csv`
+                )
+            ),
+            header: [
+                {
+                    id: 'id',
+                    title: 'Id',
+                },
+                {
+                    id: 'name',
+                    title: 'Name',
+                },
+                {
+                    id: 'connectionstring',
+                    title: 'Connection string',
+                },
+                {
+                    id: 'type',
+                    title: 'Type',
+                },
+            ],
+            append: false,
+        });
+
+        const qrsInstance = new QrsInteract({
+            hostname: config.get('ButlerSpyglass.configQRS.host'),
+            portNumber: config.get('ButlerSpyglass.configQRS.port'),
+            headers: config.get('ButlerSpyglass.configQRS.headers'),
+            certificates: {
+                certFile: config.get('ButlerSpyglass.cert.clientCert'),
+                keyFile: config.get('ButlerSpyglass.cert.clientCertKey'),
+            },
+        });
+
+        // Get info about all data connections
+        try {
+            const dataconnections = await qrsInstance.Get('dataconnection');
+            logger.debug(`DATA CONNECTION: Got response: ${dataconnections.statusCode}`);
+
+            // Save CSV to disk
+            dataconnectionsWriter
+                .writeRecords(dataconnections.body)
+                .then(() => {
+                    logger.verbose(
+                        `Done writing ${dataconnections.length} data connection records to CSV disk file`
+                    );
+                })
+                .catch((error) => {
+                    logger.error(
+                        `Failed to write data connections to CSV file on disk (make sure the output directory exists!): ${error}`
+                    );
+                    process.exit(1);
+                });
+
+            // Save JSON to disk
+            try {
+                fs.writeFileSync(
+                    path.resolve(
+                        path.normalize(
+                            `${config.get(
+                                'ButlerSpyglass.dataconnection.exportDir'
+                            )}/dataconnections.json`
+                        )
+                    ),
+                    JSON.stringify(dataconnections.body, 0, 2)
+                );
+                logger.verbose(`Done writing data connections to JSON file on disk.`);
+            } catch (err) {
+                logger.error(
+                    `Failed to write data connection file to JSON file on disk (make sure the output directory exists!): ${err}`
+                );
+                process.exit(3);
+            }
+
+            logger.info('Done writing data connection metadata to disk');
+        } catch (err) {
+            logger.error(
+                `DATA CONNECTION: Error while getting data connections: ${JSON.stringify(
+                    err,
+                    null,
+                    2
+                )}`
+            );
+            const newLocal = 'Error while getting data connections';
+            throw newLocal;
+        }
+    } catch (err) {
+        logger.error(
+            `DATA CONNECTION: Error while conecting to QRS: ${JSON.stringify(err, null, 2)}`
+        );
+    }
+};
